c7fafea3068a21b2beed827e656056eb58500e99
joshtrick <joshtrick@outlook.com>
2020-02-21 14:46:11 +0800

    train ok

--- rainlander/core/train/detectionscreen.py
+++ rainlander/core/train/detectionscreen.py
@@ -5,6 +5,7 @@ import time
 import threading
 import signal
 import subprocess
+from datetime import datetime
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -34,6 +35,7 @@ class DetectionScreen(Screen):
                 'Tiny-Yolo'         : "yolo"
                 }
         Clock.schedule_once(self.param_init, 0)
+
     # ********Parameters Registry********
     def param_init(self, dt = 0):
         self.param_update()
@@ -44,8 +46,8 @@ class DetectionScreen(Screen):
         self.param_dict['model_name']                   = self.alias_dict[self.ids.param_model_selector.text]
         self.param_dict['num_classes']                  = int(self.ids.param_num_classes.text)
         self.param_dict['gpus']                         = self.ids.param_gpus.text
-        self.param_dict['checkpoint_path']              = os.path.join(self.ids.param_checkpoint_path.text, 'ckpt')
-        self.param_dict['train_dir']                    = self.ids.param_train_dir.text
+        self.param_dict['checkpoint_path']              = os.path.join(self.ids.param_checkpoint_path.text, "ckpt")
+        self.param_dict['train_dir']                    = self.create_project_path(path = self.ids.param_train_dir.text, name = "project")
         self.param_dict['encrypted_initial_checkpoint'] = self.ids.param_encrypted_initial_checkpoint.active
         self.param_dict['batch_size']                   = self.ids.param_batch_size.value
         self.param_dict['input_sizes']                  = [int(self.ids.param_input_w.text), int(self.ids.param_input_h.text)]
@@ -61,31 +63,48 @@ class DetectionScreen(Screen):
         self.param_dict['gray']                         = self.ids.param_gray.active
 
     def write_config(self):
-        tep = TrainPipeLine()  # 训练pipeline
+        pipe = TrainPipeLine()  # 训练pipeline
 
-        tep.model.detector.yolo.num_classes             = self.param_dict['num_classes']
-        tep.model.detector.yolo.model_scale             = 0  # yolo 目前只有一个版本
-        tep.train_config.gpus                           = self.param_dict['gpus']
-        tep.train_config.checkpoint_path                = self.param_dict['checkpoint_path']
-        tep.train_config.train_dir                      = self.param_dict['train_dir']
-        tep.train_config.encrypted_initial_checkpoint   = self.param_dict['encrypted_initial_checkpoint']
-        tep.reader.batch_size                           = self.param_dict['batch_size']
-        tep.reader.class_name_path                      = self.param_dict['class_name_path']
-        tep.reader.input_sizes.extend(self.param_dict['input_sizes'])
-        tep.reader.image_input_reader.label_path        = self.param_dict['label_path']
-        tep.reader.image_input_reader.image_dir         = self.param_dict['image_dir']
+        pipe.model.detector.yolo.num_classes             = self.param_dict['num_classes']
+        pipe.model.detector.yolo.model_scale             = 0
+        pipe.train_config.gpus                           = self.param_dict['gpus']
+        pipe.train_config.checkpoint_path                = self.param_dict['checkpoint_path']
+        pipe.train_config.train_dir                      = self.param_dict['train_dir']
+        pipe.train_config.encrypted_initial_checkpoint   = self.param_dict['encrypted_initial_checkpoint']
+        pipe.reader.batch_size                           = self.param_dict['batch_size']
+        pipe.reader.class_name_path                      = self.param_dict['class_name_path']
+        pipe.reader.input_sizes.extend(self.param_dict['input_sizes'])
+        pipe.reader.image_input_reader.label_path        = self.param_dict['label_path']
+        pipe.reader.image_input_reader.image_dir         = self.param_dict['image_dir']
 
-        tep.reader.preprocessing.random_translate       = self.param_dict['random_translate']
-        tep.reader.preprocessing.random_crop            = self.param_dict['random_crop']
-        tep.reader.preprocessing.random_horizontal_flip = self.param_dict['random_horizontal_flip']
-        #tep.reader.preprocessing.random_vertical_flip   = self.param_dict['random_vertical_flip']
-        #tep.reader.preprocessing.random_rotation        = self.param_dict['random_rotation']
-        #tep.reader.preprocessing.random_shuffle_channel = self.param_dict['random_shuffle_channel']
-        #tep.reader.preprocessing.gray                   = self.param_dict['gray']
+        pipe.reader.preprocessing.random_translate       = self.param_dict['random_translate']
+        pipe.reader.preprocessing.random_crop            = self.param_dict['random_crop']
+        pipe.reader.preprocessing.random_horizontal_flip = self.param_dict['random_horizontal_flip']
+        #pipe.reader.preprocessing.random_vertical_flip   = self.param_dict['random_vertical_flip']
+        #pipe.reader.preprocessing.random_rotation        = self.param_dict['random_rotation']
+        #pipe.reader.preprocessing.random_shuffle_channel = self.param_dict['random_shuffle_channel']
+        #pipe.reader.preprocessing.gray                   = self.param_dict['gray']
 
-        # 序列化该配置为文本文件(config)
-        with open("workspace/config_tmp/train.config", 'w') as fp:
-          fp.write(text_format.MessageToString(tep))
+        # Create projects
+        project_path = self.param_dict['train_dir']
+        os.makedirs(project_path)
+        train_config_path = os.path.join(project_path, "train.config")
+        with open(train_config_path, 'w') as fp:
+          fp.write(text_format.MessageToString(pipe))
+        return train_config_path
+
+    def create_project_path(self, path, name):
+        date_time = datetime.now()
+        timestamp = str(date_time.year) \
+                + "{:02d}".format(date_time.month) \
+                + "{:02d}".format(date_time.day) \
+                + '_' \
+                + "{:02d}".format(date_time.hour) \
+                + "{:02d}".format(date_time.minute) \
+                + "{:02d}".format(date_time.second)
+        project_name = name + "_" + timestamp
+        project_path = os.path.join(path, project_name)
+        return project_path
 
 
     def param_print(self):
@@ -134,13 +153,16 @@ class DetectionScreen(Screen):
     def set_config(self):
         self.param_update()
         #self.param_print()
-        self.write_config()
-        pass
+        return self.write_config()
 
     def train(self):
-        self.set_config()
         if self.train_process == None:
-            self.train_process = subprocess.Popen(['python', 'utils/run_config.py'])
+            input_configuration = self.set_config()
+            self.train_process = subprocess.Popen([ \
+                    'python', \
+                    'utils/run_config.py', \
+                    '-t', 'train', \
+                    '-i', input_configuration])
         else:
             print("Already running")
 
--- rainlander/utils/run_config.py
+++ rainlander/utils/run_config.py
@@ -1,17 +1,36 @@
 # encoding: utf-8
-from google.protobuf import text_format
 import sys
+import argparse
+
+from google.protobuf import text_format
 from crtrain.protos.pipeline_pb2 import TrainPipeLine
+from crtrain.protos.pipeline_pb2 import EvalPipeLine
 from crtrain.api import gui_run
 
 def main():
-    tep = TrainPipeLine()  # 训练pipeline
+    parser = argparse.ArgumentParser(description='Run with a configuration')
+    parser.add_argument('-t',
+            action = 'store',
+            dest = 'task',
+            help = 'task: "train" or "eval"',
+            required = True)
+    parser.add_argument('-i',
+            action = 'store',
+            dest = 'input',
+            help = 'Input Configuration',
+            required = True)
+    args = parser.parse_args()
+
+    if args.task == 'train':
+        pipe = TrainPipeLine()  # 训练pipeline
+    elif args.task == 'eval':
+        pipe = EvalPipeLine()
 
     # 解析文本形式的配置文件
-    with open("workspace/config_tmp/train.config", "r") as fp:
-        text_format.Merge(fp.read(), tep)
+    with open(args.input, "r") as fp:
+        text_format.Merge(fp.read(), pipe)
 
-    gui_run(tep)
+    gui_run(pipe)
 
 if __name__ == '__main__':
     main()
